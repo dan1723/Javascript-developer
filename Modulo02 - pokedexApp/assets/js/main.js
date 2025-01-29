b4f0c8cd5d26e5c65a51ff05dab3c59bf4a1188c
Danilo Fernandes <dan.gomes1723@gmail.com>
2025-01-29 16:58:28 -0300

    2º commit

--- Modulo02 - pokedexApp/assets/js/main.js
+++ Modulo02 - pokedexApp/assets/js/main.js
@@ -1,1 +1,42 @@
-console.log('Sucesso.');
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+
+function convertPokemonToHtml(pokemon) {
+    return `
+    <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                </div>
+            </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+pokemonList.innerHTML += '<li>Teste</li>'
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            console.log(convertPokemonToHtml(pokemon))
+
+            
+        }
+
+    })
+    .catch((error) => console.error(error))
+
+
