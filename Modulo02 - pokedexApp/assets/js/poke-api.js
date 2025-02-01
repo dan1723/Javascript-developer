d3b80e013fd347298b3b183a8d96fd341640fb5e
Danilo Fernandes <dan.gomes1723@gmail.com>
2025-01-31 21:02:28 -0300

    Implementando a pokeiApi

--- Modulo02 - pokedexApp/assets/js/poke-api.js
+++ Modulo02 - pokedexApp/assets/js/poke-api.js
@@ -1,12 +1,34 @@
 
-
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then((pokemon) => {
+
+        })
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error))
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
+
+
+
+
+
+
+
+
+
