f3b1414df159745a0297cbad5b0c90028f6de64a
Danilo Fernandes <dan.gomes1723@gmail.com>
2025-03-08 16:30:03 -0300

    estudando javascript

---
+++ tipos-dados.js
@@ -0,0 +1,10 @@
+function falarMeuNome() {
+    console.log('Meu nome é Danilo')
+}
+
+function falarMeuNomeCompleto(falarMeuNome){
+    falarMeuNome()
+    console.log('Danilo Fernandes Gomes')
+}
+
+falarMeuNomeCompleto(falarMeuNome)
